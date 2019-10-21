19e715278a13d72e86d5736a0b8def5f7871db02
98o718 <kalyakin.maks@mail.ru>
2019-10-21 17:06:08 +0300

    Create UserCredentialDto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { AuthService } from './auth.service'
 import { ApiUseTags, ApiBearerAuth, ApiImplicitBody } from '@nestjs/swagger'
-import { CreateUserDto } from '../users/dto/create-user.dto'
+import { UserCredentials } from './dto/user-credentials.dto'
 
 @Controller('auth')
 @ApiUseTags('Auth')
@@ -12,7 +12,7 @@ export class AuthController {
   @UseGuards(AuthGuard('local'))
   @ApiImplicitBody({
     name: 'User credentials',
-    type: CreateUserDto,
+    type: UserCredentials,
     required: true,
   })
   @Post('signin')
