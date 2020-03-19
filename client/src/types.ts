3c22fe120318a602af282da24d2e9e41c21647c6
98o718 <kalyakin.maks@mail.ru>
2020-03-19 15:10:18 +0300

    Add recipe interface

--- client/src/types.ts
+++ client/src/types.ts
@@ -20,3 +20,51 @@ export type RecipeDescription = {
   readonly recipeType: RecipeType
   readonly access: RecipeAccessType
 }
+
+export interface Grain {
+  name: string
+  weight: number
+  color?: number
+}
+
+export interface Hop {
+  name: string
+  weight: number
+  alpha: number
+  hopType?: string
+  time: number
+}
+
+export interface Yeast {
+  name: string
+  weight: number
+  temp: number
+}
+
+export interface Pause {
+  name?: string
+  temp: number
+  time: number
+}
+
+export interface Recipe {
+  title: string
+  description?: string
+  details?: string
+  ingredients: {
+    grains: Grain[]
+    hops: Hop[]
+    yeast: Yeast | null
+  }
+  style?: string
+  pauses: Pause[]
+  volume: number
+  abv: number
+  ibu: number
+  og: number
+  fg: number
+  type: RecipeType
+  rating?: number
+  access?: RecipeAccessType
+  url?: string
+}
