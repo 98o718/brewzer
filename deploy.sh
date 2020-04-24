495155f94d5d3404b69054a27da0b9ad6e4c32cd
98o718 <kalyakin.maks@mail.ru>
2020-04-24 19:21:19 +0300

    Update deploy script for service-worker build

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -xe
 
 cd client
 yarn
-CI=false yarn build
+CI=false yarn build-sw
 rsync -rq --delete --rsync-path="mkdir -p brewzer-client && rsync" \
 $TRAVIS_BUILD_DIR/client/build/ travis@51.15.124.103:brewzer-client
 npm install -g pm2
