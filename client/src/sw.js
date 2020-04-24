f00ea0931afd7b82180ba085d8a00bc32042d097
98o718 <kalyakin.maks@mail.ru>
2020-04-24 19:54:47 +0300

    Update SW console logs

--- client/src/sw.js
+++ client/src/sw.js
@@ -2,12 +2,6 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js',
 )
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`)
-} else {
-  console.log(`Boo! Workbox didn't load 😬`)
-}
-
 // SETTINGS
 
 // Path prefix to load modules locally
